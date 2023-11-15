32ec1651cf3cb91b8ba8f402e38218478fc00223
Bieuzitos <34816931+bieuzitos@users.noreply.github.com>
2023-11-15 11:48:41 -0300

    [UPDATE] Date format

--- resources/views/home/scripts/create.php
+++ resources/views/home/scripts/create.php
@@ -204,31 +204,4 @@
             $(this).text(diffForHumans($(this).data('updated')))
         })
     }
-
-    function diffForHumans(updated_at) {
-        const current = new Date()
-        const updated = new Date(updated_at)
-
-        const diffInMilliseconds = updated - current;
-        const diffInSeconds = Math.floor(Math.abs(diffInMilliseconds) / 1000);
-
-        if (diffInSeconds < 60) {
-            return `há ${diffInSeconds} segundo${diffInSeconds !== 1 ? 's' : ''}`;
-        } else if (diffInSeconds < 3600) {
-            const minutes = Math.floor(diffInSeconds / 60);
-            return `há ${minutes} minuto${minutes !== 1 ? 's' : ''}`;
-        } else if (diffInSeconds < 86400) {
-            const hours = Math.floor(diffInSeconds / 3600);
-            return `há ${hours} hora${hours !== 1 ? 's' : ''}`;
-        } else if (diffInSeconds < 2592000) {
-            const days = Math.floor(diffInSeconds / 86400);
-            return `há ${days} dia${days !== 1 ? 's' : ''}`;
-        } else if (diffInSeconds < 31536000) {
-            const months = Math.floor(diffInSeconds / 2592000);
-            return `há ${months} mês${months !== 1 ? 'es' : ''}`;
-        } else {
-            const years = Math.floor(diffInSeconds / 31536000);
-            return `há ${years} ano${years !== 1 ? 's' : ''}`;
-        }
-    }
 </script>
