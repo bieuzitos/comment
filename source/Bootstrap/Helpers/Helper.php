7818f11ab5734fe1fb46161af9d9adc29bc3ce8b
Bieuzitos <34816931+bieuzitos@users.noreply.github.com>
2023-12-28 15:01:57 -0300

    [UPDATE] Basic information

--- source/Bootstrap/Helpers/Helper.php
+++ source/Bootstrap/Helpers/Helper.php
@@ -1,11 +1,39 @@
 <?php
 
 /**
+ * @return bool
+ */
+function is_localhost(): bool
+{
+    $whitelist = ['127.0.0.1', '::1'];
+    if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
  * @return string
  */
 function url(): string
 {
-    return URL_TEST;
+    if (is_localhost()) {
+        return URL_TEST;
+    }
+
+    return URL_BASE;
+}
+
+
+/**
+ * @param string|null $url
+ * 
+ * @return string
+ */
+function url_replace(?string $url = null): string
+{
+    return str_replace('https://www.', '', $url ?? url());
 }
 
 /**
